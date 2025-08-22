0b8605d6bd5dfb44e4de3b180ee3d49707cff2f8
Екатерина Орлова <ekaterinaorlova@MacBook-Air-Ekaterina.local>
2025-08-22 20:28:18 +0300

    Добавлен hook.php с переменными окружения

--- hook.php
+++ hook.php
@@ -1,25 +1,29 @@
 <?php
-// === Настройки ===
-$TELEGRAM_TOKEN = "7298042093:AAGDuO6pU-AuH_Lz01oL1evByDp4yetBKuo"; // 
-твой токен
-$TELEGRAM_CHAT_ID = "5113963562"; // твой chat_id
+// === Настройки через переменные окружения ===
+$TELEGRAM_TOKEN = getenv('BOT_TOKEN'); 
+$TELEGRAM_CHAT_ID = getenv('CHAT_ID'); 
 
 // Тестовое сообщение
 $text = "Тестовое уведомление от бота";
 
+// Формируем URL и POST-поля
 $url = "https://api.telegram.org/bot{$TELEGRAM_TOKEN}/sendMessage";
 $post = ['chat_id' => $TELEGRAM_CHAT_ID, 'text' => $text];
 
+// Отправляем сообщение через CURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
+
+// Проверяем ошибки
 if (curl_errno($ch)) {
     echo "CURL Error: " . curl_error($ch);
 } else {
     var_dump($response); // Проверка ответа Telegram
 }
+
 curl_close($ch);
 
